fde384cf1bd5f49e9bc99587cf97433ec62646bc
Harmjan Treep <mail@harmjantreep.nl>
2014-11-11 00:01:36 +0100

    Add structure for storing and drawing windows.
    A test for a javascript Container class. Tiling
    in different containers seems to work, it is now
    hardcoded that the second window gets turned into
    a vertical container after creation so that you
    can test the functionality a bit.

--- airwm.js
+++ airwm.js
@@ -1,9 +1,123 @@
 var x11 = require('x11');
 
+// The number of pixels between each window and the border
+var margin = 10;
+
 var X;
 var events = x11.eventMask.SubstructureRedirect|x11.eventMask.SubstructureNotify|x11.eventMask.SubstructureRedirect|x11.eventMask.Exposure;
 
-var windows = [];
+/**
+ * Create a new container in the window tree.
+ */
+function Container( horizontal, x, y, width, height ) {
+	this.horizontal = horizontal;
+	this.x          = x;
+	this.y          = y;
+	this.width      = width;
+	this.height     = height;
+	this.windows    = [];
+
+	/**
+	 * Add a window to this container.
+	 * \param wid The window id of the window to add.
+	 */
+	this.add_window = function(wid) {
+		this.windows.push(wid);
+		this.recalculate();
+	};
+
+	/**
+	 * Remove a window from this container.
+	 * \param wid The window id of the window to remove.
+	 */
+	this.remove_window = function(wid) {
+		for( var i=0; i<this.windows.length; ++i ) {
+			if( typeof this.windows[i] === "number" ) {
+				if( this.windows[i] === wid ) {
+					this.windows.splice(this.windows.indexOf(wid),1);
+					this.recalculate();
+				}
+			}
+			else {
+				this.windows[i].remove_window(wid);
+				if( this.windows[i].windows.length === 0 ) {
+					this.windows.splice(i,1);
+					this.recalculate();
+				}
+			}
+		}
+	};
+
+	/**
+	 * Add a container to this container.
+	 * \param horizontal If the new container tiles windows horizontally
+	 *                   or vertically.
+	 * \param wid The window id to turn into a new container.
+	 */
+	this.add_container = function(horizontal,wid) {
+		for( var i=0; i<this.windows.length; ++i ) {
+			if( this.windows[i] === wid ) {
+				this.windows[i] = new Container( horizontal, 0, 0, 1, 1 );
+				this.windows[i].add_window( wid );
+				this.recalculate();
+				break;
+			}
+		}
+	};
+
+	/**
+	 * Recalculate and move all windows of this and all containers
+	 * below it to the correct place.
+	 */
+	this.recalculate = function() {
+		if( this.horizontal ) {
+			// The width of a window after removing the margins
+			var window_width = parseInt((this.width-(this.windows.length-1)*margin) / this.windows.length);
+			for( var i=0; i<this.windows.length; ++i ) {
+				if( typeof this.windows[i] === "number" ) {
+					X.MoveResizeWindow(
+						this.windows[i],
+						this.x + i*(margin+window_width),
+						this.y,
+						window_width,
+						this.height );
+				}
+				else {
+					this.windows[i].x      = this.x + i*(margin+window_width);
+					this.windows[i].y      = this.y;
+					this.windows[i].width  = window_width;
+					this.windows[i].height = this.height;
+					this.windows[i].recalculate();
+				}
+			}
+		}
+		else {
+			// The height of a window after removing the margins
+			var window_height = parseInt((this.height-(this.windows.length-1)*margin) / this.windows.length);
+			for( var i=0; i<this.windows.length; ++i ) {
+				if( typeof this.windows[i] === "number" ) {
+					X.MoveResizeWindow(
+						this.windows[i],
+						this.x,
+						this.y + i*(margin+window_height),
+						this.width,
+						window_height);
+				}
+				else {
+					this.windows[i].x      = this.x;
+					this.windows[i].y      = this.y + i*(margin+window_width);
+					this.windows[i].width  = this.width;
+					this.windows[i].height = window_height;
+					this.windows[i].recalculate();
+				}
+			}
+		}
+	};
+}
+
+var window_tree = new Container( true, margin, margin, 800-2*margin, 600-2*margin );
+
+var counter = 0;
 
 x11.createClient(function(err, display) {
 	X = display.client;
@@ -15,35 +129,35 @@ x11.createClient(function(err, display) {
 }).on('error', function(err) {
 	console.error(err);
 }).on('event', function(ev) {
-	console.log(ev);
+	//console.log(ev);
 	if( ev.type === 20 ) {
-		// Record that this window exists
-		windows.push( ev.wid );
+		// To show that the container works change the second
+		// created window into a vertical container and add
+		// future windows to that contaner.
+		++counter;
+
+		if( counter <= 2 ) {
+			// Record that this window exists
+			window_tree.add_window( ev.wid );
+
+			if( counter == 2 ) {
+				window_tree.add_container( false, ev.wid );
+			}
+		}
+		else {
+			window_tree.windows[1].add_window( ev.wid );
+		}
+
+		console.log( "Current window tree: ", window_tree );
 
 		// Tell X to map this window
 		X.MapWindow( ev.wid );
-    resize();
 	} else if ( ev.type === 17 ) {
-    windows.splice(windows.indexOf(ev.wid),1);
-    resize();
-    //X.DestroyWindow( ev.wid );
-  } else if ( ev.type === 23 ) {
-    var window_width = parseInt(800.0 / windows.length);
-    X.ResizeWindow(ev.wid, window_width, ev.height);
-  }
+		window_tree.remove_window( ev.wid );
+		console.log( "Current window tree: ", window_tree );
+		//X.DestroyWindow( ev.wid );
+	} else if ( ev.type === 23 ) {
+		//var window_width = parseInt(800.0 / windows.length);
+		X.ResizeWindow(ev.wid, ev.width, ev.height);
+	}
 });
-
-function resize(){
-		// Update all windows
-    console.log("Current list of Windows:", windows)
-		for( var i=0; i<windows.length; i++ ) {
-			var window_width = parseInt(800.0 / windows.length);
-			var margin = 10;
-			X.MoveResizeWindow(
-				windows[i],
-				window_width * i+margin,
-				0+margin,
-				window_width-2*margin,
-				600-2*margin );
-		}
-}
