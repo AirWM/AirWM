4dec6a6bbf4d9957a863f5fc695cc335beba1a3a
Niels Spruit <niellus16@hotmail.com>
2014-12-08 11:07:07 +0100

    Now,we have key bindings to switch forward and backwards

--- airwm.js
+++ airwm.js
@@ -119,6 +119,10 @@ x11.createClient(function(err, display) {
 								console.log("Switching tiling mode");
 								workspaces.getCurrentWorkspace().switchTilingMode();
 								break;
+							case "SwitchWorkspace":
+								console.log("Switching workspace");
+								workspaces.moveRight();
+								break;
 							default:
 								break;
 						}
